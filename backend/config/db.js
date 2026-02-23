f2cbb2734a09ad4070bab5dd4826f36f8f74638e
prince <princeprajapati3848@gmail.com>
2026-02-23 10:00:24 +0530

    CICD detup

--- backend/config/db.js
+++ backend/config/db.js
@@ -12,8 +12,6 @@ export const connectDb = async () => {
 
 
 
-//mongodb://princeprajapati3848:princeFoodDelivery@cluster0-shard-00-00.8r8l0.mongodb.net:27017,cluster0-shard-00-01.8r8l0.mongodb.net:27017,cluster0-shard-00-02.8r8l0.mongodb.net:27017/food-delivery?ssl=true&replicaSet=atlas-xxxxx&authSource=admin&retryWrites=true&w=majority
-//mongodb+srv://princeprajapati3848:princeFoodDelivery@cluster0.8r8l0.mongodb.net/food-delivery
 
 
 
