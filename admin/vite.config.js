9367aabea6e9cd27ce3ea652e74606e34c0dd843
Ubuntu <ubuntu@ip-172-31-1-115.ap-south-1.compute.internal>
2026-09-23 11:12:54 +0000

    Fix DockerFile

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -4,5 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/princeadmin"
+  base: "/admin/",
+  server: {
+    host: "0.0.0.0",
+    port: 5174,
+    allowedHosts: true
+  }
 })
